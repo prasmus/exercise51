ff5aedbb969c6f04c315a308e5a0b2a8d0c103cf
Upsteem <Upsteem>
2016-11-10 11:19:11 +0200

    Initial commit

---
+++ src/exercise51.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Praktika on 10.11.2016.
+ */
+public class exercise51 {
+    public static void main(String[] args) {
+
+    if (x > 0) && (x < 10) {
+            System.out.println("positive single digit number.");
+        }
+    }
+}
